5dae44ecc0b55301867484a6a4ab5ba69f88a81d
Xueqi Fan <fanxueqi77@gmail.com>
2019-10-01 12:53:29 -0700

    first!

---
+++ case.php
@@ -0,0 +1,53 @@
+<?php
+  if (isset($_GET["case"])) {
+    $case = $_GET["case"];
+    $result= load_case_data($case);
+    header("Content-type: application/json");
+    print(json_encode($result));
+  } else if (isset($_GET["classification"])) {
+    $classification = $_GET["classification"];
+    $result = load_classification_data($classification);
+    header("Content-type: application/json");
+    print(json_encode($result));
+  } else {
+    output_error("Baby you need to pass a parameter!");
+  }
+
+  function load_case_data($case) {
+    $result = array();
+    $file_name = "cases/" . $case . "/info.txt";
+    $infoContent = file($file_name, FILE_IGNORE_NEW_LINES);
+    foreach ($infoContent as $line) {
+      list($part1,$part2) = explode(':', $line, 2);
+      if (strpos($part2, '|') !== false) {
+        $part2 = explode('|', $part2);
+      }
+      $result[$part1] = $part2;
+    }
+    return $result;
+  }
+
+  function load_classification_data($classification) {
+    $result = array();
+    $file_name = "classification/".$classification."/classes.txt";
+    $classes = file($file_name, FILE_IGNORE_NEW_LINES);
+    foreach ($classes as $class) {
+      list($part1, $part2) = explode(':', $class);
+      if ($part2 != "") {
+        $cases = explode(',', $part2);
+        $result[$part1] = $cases;
+      } else {
+        $result[$part1] = [];
+      }
+    }
+    return $result;
+  }
+
+
+  function output_error($msg) {
+    header("HTTP/1.1 400 Invalid Request");
+    header("Content-type: text/plain");
+    /* Otherwise we have an error, and should say so. */
+    echo $msg;
+  }
+?>
---
+++ main.js
@@ -0,0 +1,167 @@
+(function(){
+  "use strict";
+
+  const URL = "case.php";
+  const CRITERIAS = {
+    "application" : ["external", "internal", "intergrated"],
+    "control": ["center", "local", "centern and local", "no control"],
+    "movement": ["fold", "slide", "rotate", "twist", "roll"],
+    "sensors": ["light", "temperature", "moisture", "touch", "acceleration"]
+  };
+
+  window.addEventListener("load", init);
+
+  function init() {
+    createNav();
+  }
+
+  function createNav() {
+    for (var key in CRITERIAS) {
+      if (CRITERIAS.hasOwnProperty(key)) {
+        let btn = document.createElement("BUTTON");
+        btn.innerText = key;
+        btn.id = key;
+        btn.addEventListener("click", function() {showClassifications(btn.id)});
+        id("criterias").appendChild(btn);
+      }
+    }
+  }
+
+  function showClassifications(key) {
+    clearOldClass();
+    id(key).classList.add("selected");
+    let classHere = fetchClassificationData(key);
+  }
+
+  function clearOldClass() {
+    let list = id("classifications");
+    while (list.hasChildNodes()) {
+      list.removeChild(list.firstChild);
+    }
+
+    let select = qs(".selected");
+    if (select != null) {
+      select.classList.remove("selected");
+    }
+  }
+
+  /**
+   * Fetches and displays classification data, for example: control
+   */
+  function fetchClassificationData(key) {
+    let url = URL + "?classification=" + key;
+
+    fetch(url)
+       .then(checkStatus)
+       .then(JSON.parse)
+       .then(populatePage)
+       .catch(() => {
+         console.log("a!")
+       });
+  }
+
+
+  /**
+  * Fetches and displays the cases under classifications
+  */
+  function populatePage(response) {
+    let numClasses = Object.keys(response).length;
+    for (let i = 0; i < numClasses; i++) {
+      let whatclass = Object.keys(response)[i];
+
+      let classification = document.createElement("div");
+      classification.classList.add("classification");
+      classification.width = window.innerWidth / numClasses;
+      console.log(window.innerWidth);
+      console.log(classification.width);
+
+      let classTitle = document.createElement("p");
+      classTitle.innerText = whatclass;
+      classification.appendChild(classTitle);
+
+      let projects = document.createElement("div");
+      projects.id = whatclass;
+      projects.classList.add("projectsContainer");
+      classification.appendChild(projects);
+
+      id("classifications").appendChild(classification);
+
+      for (let j = 0; j < response[whatclass].length; j++) {
+        let project = response[whatclass][j];
+
+        let icon = document.createElement("img");
+        icon.classList.add("icon");
+        icon.src = "cases/" + project + "/icon.gif";
+        icon.addEventListener("click", function() {showProjectDetail(project)});
+        id(whatclass).appendChild(icon);
+      }
+    }
+
+    /**
+     * Fetches and displays project Detail
+     */
+    function showProjectDetail(project) {
+      let projectDetail = fetchProjectDetailData();
+    }
+
+    /**
+     * Fetches the project detail data, including introduction, location, architects...
+     */
+    function fetchClassificationData(key) {
+      let url = URL + "?classification=" + key;
+
+      fetch(url)
+         .then(checkStatus)
+         .then(JSON.parse)
+         .then(populatePage)
+         .catch(() => {
+           console.log("a!")
+         });
+    }
+  }
+
+  /* ------------------------------ Helper Functions  ------------------------------ */
+  // Note: You may use these in your code, but do remember that your code should not have
+  // any functions defined that are unused.
+
+  /**
+   * Returns the element that has the ID attribute with the specified value.
+   * @param {string} idName - element ID
+   * @returns {object} DOM object associated with id.
+   */
+  function id(idName) {
+    return document.getElementById(idName);
+  }
+
+  /** return the element with the given selector
+   * @param {String} selector - selector of the specific html elements
+   * @return {element} returns html element of given selector
+  */
+  function qsa(selector) {
+    return document.querySelectorAll(selector);
+  }
+
+  /** return the element with the given selector
+   * @param {String} selector - selector of the specific html elements
+   * @return {element} returns html element of given selector
+  */
+  function qs(selector) {
+    return document.querySelector(selector);
+  }
+
+  /**
+   * Helper function to return the response's result text if successful, otherwise
+   * returns the rejected Promise result with an error status and corresponding text
+   * @param {object} response - response to check for success/error
+   * @returns {object} - valid result text if response was successful, otherwise rejected
+   *                     Promise result
+   */
+  function checkStatus(response) {
+    if (response.status >= 200 && response.status < 300 || response.status === 0) {
+      return response.text();
+    } else {
+      return Promise.reject(new Error(response.status + ": " + response.statusText));
+    }
+  }
+
+})();
